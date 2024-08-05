6f983b860ccb2030bed7272faca4c131e3e07712
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-05 20:20:56 +0300

    user client interceptor

---
+++ postservice/src/main/java/com/clone/instagram/postservice/client/UserServiceClient.java
@@ -0,0 +1,20 @@
+package com.clone.instagram.postservice.client;
+
+import com.clone.instagram.postservice.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+public interface UserServiceClient {
+
+    @GetMapping("/users/{userId}")
+    UserDto getUser(@PathVariable long userId);
+
+    @PostMapping("/users")
+    List<UserDto> getUsersByIds(@RequestBody List<Long> ids);
+}
