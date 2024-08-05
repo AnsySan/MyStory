e3a1850212502bb0f9692b6a6a3a1423c0953f46
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-05 20:20:01 +0300

    controller

---
+++ postservice/src/main/java/com/clone/instagram/postservice/controller/LikeController.java
@@ -0,0 +1,30 @@
+package com.clone.instagram.postservice.controller;
+
+import com.clone.instagram.postservice.dto.LikeDto;
+import com.clone.instagram.postservice.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/post/{postId}")
+    @Operation(summary = "Like post")
+    public LikeDto likePost(@RequestBody @Validated LikeDto likeDto) {
+        return likeService.likePost(likeDto);
+    }
+
+    @DeleteMapping("/post/{postId}")
+    @Operation(summary = "Remove like from post")
+    public void deleteLikeFromPost(@Valid @PathVariable Long postId) {
+        likeService.deleteLikeFromPost(postId);
+    }
+}
