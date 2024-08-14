40e1424788bbf95fdef998c2e09207e672bc5c89
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-14 21:13:44 +0300

    Introduced user context filter for constant twitter-user-id header parsing for every request

---
+++ userservice/src/main/java/com/clone/instagram/userservice/context/UserContext.java
@@ -0,0 +1,21 @@
+package com.clone.instagram.userservice.context;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContext {
+
+    private final ThreadLocal<Integer> userIdHolder = new ThreadLocal<>();
+
+    public void setUserId(int userId) {
+        userIdHolder.set(userId);
+    }
+
+    public long getUserId() {
+        return userIdHolder.get();
+    }
+
+    public void clear() {
+        userIdHolder.remove();
+    }
+}
