2af62c3dfd6ce1924fd3764c7b5715db3458e039
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-14 21:13:22 +0300

    Update Config

---
+++ userservice/src/main/java/com/clone/instagram/userservice/config/ExecutorConfig.java
@@ -0,0 +1,21 @@
+package com.clone.instagram.userservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ExecutorConfig {
+
+    @Value("${executorConfig.thread-pool-size}")
+    private int threadPoolSize;
+
+
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(threadPoolSize);
+    }
+}
