8f58f6e22474a52136bcb356305db19368762b03
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-17 10:05:17 +0300

    first commit

---
+++ userservice/src/main/java/com/clone/twitter/userservice/entity/ContactType.java
@@ -0,0 +1,5 @@
+package com.clone.twitter.userservice.entity;
+
+public enum ContactType {
+    GITHUB, TELEGRAM, VK, FACEBOOK, INSTAGRAM, WHATSAPP, CUSTOM
+}
---
+++ userservice/src/main/java/com/clone/twitter/userservice/entity/User.java
@@ -0,0 +1,91 @@
+package com.clone.twitter.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", length = 64, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", length = 64, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", length = 32, unique = true)
+    private String phone;
+
+    @Column(name = "password", length = 128, nullable = false)
+    private String password;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Column(name = "about_me", length = 4096)
+    private String aboutMe;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(name = "city", length = 64)
+    private String city;
+
+    @Column(name = "experience")
+    private Integer experience;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(name = "subscription",
+            joinColumns = @JoinColumn(name = "followee_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<User> followers;
+
+    @ManyToMany(mappedBy = "followers")
+    private List<User> followees;
+
+    @OneToMany(mappedBy = "user")
+    private List<Contact> contacts;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "fileId", column = @Column(name = "profile_pic_file_id")),
+            @AttributeOverride(name = "smallFileId", column = @Column(name = "profile_pic_small_file_id"))
+    })
+    private UserProfilePicture userProfilePic;
+
+    @OneToOne(mappedBy = "user")
+    private ContactPreference contactPreference;
+
+    @Column(name = "banned", nullable = false)
+    private boolean banned;
+
+    public long getId() {
+        return id;
+    }
+}
