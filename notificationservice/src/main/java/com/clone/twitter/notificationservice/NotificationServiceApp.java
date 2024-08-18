c8ed16dbce504960517650d5ab2cb92c1e36e9ba
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-18 14:29:06 +0300

    first commit

---
+++ notificationservice/src/main/java/com/clone/twitter/notificationservice/NotificationServiceApp.java
@@ -0,0 +1,24 @@
+package com.clone.twitter.notificationservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients("com.clone.twitter.notificationservice.client")
+public class NotificationServiceApp {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(NotificationServiceApp.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
---
+++ urlshortenerservice/src/main/java/com/clone/twitter/urlshortenerservice/UrlshortenerserviceApplication.java
@@ -0,0 +1,28 @@
+package com.clone.twitter.urlshortenerservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableFeignClients("com.clone.twitter.urlshortenerservice.client")
+public class UrlshortenerserviceApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(UrlshortenerserviceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
