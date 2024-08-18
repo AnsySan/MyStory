0ad0697bda8404ad6947b4a856cfa83a1b800982
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-18 19:16:20 +0300

    Async config

---
+++ postservice/src/main/java/com/clone/twitter/postservice/config/AsyncConfig.java
@@ -0,0 +1,46 @@
+package com.clone.twitter.postservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Value("${async.corePoolSize}")
+    private int corePoolSize;
+
+    @Value("${async.maxPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${async.queueCapacity}")
+    private int queueCapacity;
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public ExecutorService adRemoverExecutorService(@Value("${post.ad-remover.threads-count}") int threadsCount) {
+        return Executors.newFixedThreadPool(threadsCount);
+    }
+
+    @Bean
+    public ExecutorService commentModeratorExecutorService(@Value("${moderation.threads-count}") int threadsCount) {
+        return Executors.newFixedThreadPool(threadsCount);
+    }
+}
---
+++ postservice/src/main/java/com/clone/twitter/postservice/config/ExecutorConfig.java
@@ -0,0 +1,17 @@
+package com.clone.twitter.postservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ExecutorConfig {
+
+    @Bean
+    public ExecutorService executorService(@Value("${post.moderator.threads-count}") int threadsCount) {
+        return Executors.newFixedThreadPool(threadsCount);
+    }
+}
