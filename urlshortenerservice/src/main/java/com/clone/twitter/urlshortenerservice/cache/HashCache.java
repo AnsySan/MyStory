05f497eb03a39286e4bb2edf6c93b64865e6669e
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-19 21:04:56 +0300

    HashCache

---
+++ urlshortenerservice/src/main/java/com/clone/twitter/urlshortenerservice/cache/HashCache.java
@@ -0,0 +1,50 @@
+package com.clone.twitter.urlshortenerservice.cache;
+
+import com.clone.twitter.urlshortenerservice.entity.Hash;
+import com.clone.twitter.urlshortenerservice.generator.HashGenerator;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class HashCache {
+
+    private final HashGenerator hashGenerator;
+
+    @Value("${app.hash.cache.size}")
+    private int cacheSize;
+    @Value("${app.hash.cache.fill-percent}")
+    private int fillPercent;
+
+    private AtomicBoolean isRefilling;
+    private ArrayBlockingQueue<Hash> hashes;
+
+    @PostConstruct
+    public void init() {
+        isRefilling = new AtomicBoolean(false);
+
+        log.info("Starting init HashCache");
+        hashes = new ArrayBlockingQueue<>(cacheSize);
+        hashGenerator.getHashesAsync(cacheSize).thenAccept(hashes::addAll);
+        log.info("Finished starting init HashCache, cache size: {}", cacheSize);
+    }
+
+    public Hash getHash() {
+        if ((hashes.size() / (cacheSize / 100) < fillPercent)
+                && (isRefilling.compareAndSet(false, true))) {
+            log.info("Replenish the cache of hashes: {}", cacheSize);
+            hashGenerator.getHashesAsync(cacheSize)
+                    .thenAccept(hashes::addAll)
+                    .thenRun(() -> isRefilling.set(false));
+        }
+        log.info("Finished replenish the cache of hashes: {}", cacheSize);
+        return hashes.poll();
+    }
+}
