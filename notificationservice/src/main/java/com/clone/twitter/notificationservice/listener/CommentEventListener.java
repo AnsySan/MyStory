c361cd3885043919a23caf4e132e2d7823f16918
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-19 21:08:54 +0300

    commit listener

---
+++ notificationservice/src/main/java/com/clone/twitter/notificationservice/listener/CommentEventListener.java
@@ -0,0 +1,30 @@
+package com.clone.twitter.notificationservice.listener;
+
+import com.clone.twitter.notificationservice.client.UserServiceClient;
+import com.clone.twitter.notificationservice.dto.CommentEvent;
+import com.clone.twitter.notificationservice.message.MessageBuilder;
+import com.clone.twitter.notificationservice.service.NotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class CommentEventListener extends AbstractListener<CommentEvent> {
+    protected CommentEventListener(ObjectMapper objectMapper,
+                                   UserServiceClient userServiceClient,
+                                   MessageBuilder<CommentEvent> messageBuilder,
+                                   List<NotificationService> notificationServicesList) {
+        super(objectMapper, userServiceClient, messageBuilder, notificationServicesList);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        handleEvent(message, CommentEvent.class, commentEvent -> {
+            String textMessage = getMessage(commentEvent, Locale.getDefault());
+            sendNotification(commentEvent.getAuthorOfPostId(), textMessage);
+        });
+    }
+}
---
+++ notificationservice/src/main/java/com/clone/twitter/notificationservice/listener/LikeEventListener.java
@@ -0,0 +1,32 @@
+package com.clone.twitter.notificationservice.listener;
+
+
+import com.clone.twitter.notificationservice.client.UserServiceClient;
+import com.clone.twitter.notificationservice.dto.LikeEvent;
+import com.clone.twitter.notificationservice.message.MessageBuilder;
+import com.clone.twitter.notificationservice.service.NotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class LikeEventListener extends AbstractListener<LikeEvent> {
+
+    protected LikeEventListener(ObjectMapper objectMapper,
+                                UserServiceClient userServiceClient,
+                                MessageBuilder<LikeEvent> messageBuilder,
+                                List<NotificationService> notificationServicesList) {
+        super(objectMapper, userServiceClient, messageBuilder, notificationServicesList);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        handleEvent(message, LikeEvent.class, likeEvent -> {
+            String textMessage = getMessage(likeEvent, Locale.getDefault());
+            sendNotification(likeEvent.getAuthorLikeId(), textMessage);
+        });
+    }
+}
