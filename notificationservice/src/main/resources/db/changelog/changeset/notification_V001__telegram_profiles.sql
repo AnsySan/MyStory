2e79025142c706d48e7f2c8260c5a5c2dad3487a
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-20 16:19:38 +0300

    modified

--- notificationservice/src/main/resources/db/changelog/changeset/notification_V001__telegram_profiles.sql
+++ notificationservice/src/main/resources/db/changelog/changeset/notification_V001__telegram_profiles.sql
@@ -3,5 +3,5 @@ CREATE TABLE telegram_profiles
     id        BIGSERIAL PRIMARY KEY UNIQUE,
     user_id   BIGINT       NOT NULL,
     user_name VARCHAR(255) NOT NULL UNIQUE,
-    chat_id   BIGINT       NOT NULL UNIQUE,
+    chat_id   BIGINT       NOT NULL UNIQUE
 )
