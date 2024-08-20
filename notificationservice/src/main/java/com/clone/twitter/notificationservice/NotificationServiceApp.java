d1893902135efb235fc0db2842a230fb864cf962
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-20 16:19:01 +0300

    modified

--- notificationservice/src/main/java/com/clone/twitter/notificationservice/NotificationServiceApp.java
+++ notificationservice/src/main/java/com/clone/twitter/notificationservice/NotificationServiceApp.java
@@ -3,12 +3,13 @@ package com.clone.twitter.notificationservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableKafka
+@SpringBootApplication
 @EnableFeignClients("com.clone.twitter.notificationservice.client")
 public class NotificationServiceApp {
     public static void main(String[] args) {
