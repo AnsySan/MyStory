4421e60794b507bc2672e18d337729d9e9fea6e0
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-21 16:10:39 +0300

    modified

--- userservice/src/main/java/com/clone/twitter/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/clone/twitter/userservice/UserserviceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableScheduling
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
                 title = "User Service",
                 version = "1.0.0")
 )
-@RequestMapping("/api/v1")
 public class UserserviceApplication {
 
     public static void main(String[] args) {
