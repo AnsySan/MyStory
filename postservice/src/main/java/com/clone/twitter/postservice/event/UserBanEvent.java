b0dd9ca5c2c64365097cd99713d3e131f1ab23d8
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-23 17:21:08 +0300

    User event ban

---
+++ postservice/src/main/java/com/clone/twitter/postservice/event/UserBanEvent.java
@@ -0,0 +1,12 @@
+package com.clone.twitter.postservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserBanEvent {
+    private List<Long> userIds;
+}
