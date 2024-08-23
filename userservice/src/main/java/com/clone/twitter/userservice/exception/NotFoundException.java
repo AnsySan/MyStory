80a981986716abb2988f51084c41e38353f331ef
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-23 17:19:11 +0300

    exception

---
+++ userservice/src/main/java/com/clone/twitter/userservice/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.clone.twitter.userservice.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
