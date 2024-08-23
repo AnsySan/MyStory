ae5eda438d89941d75c94d536976dda28065a986
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-23 17:20:59 +0300

    Hash

---
+++ urlshortenerservice/src/main/java/com/clone/twitter/urlshortenerservice/dto/HashDto.java
@@ -0,0 +1,14 @@
+package com.clone.twitter.urlshortenerservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HashDto {
+    private String uniqueHash;
+}
