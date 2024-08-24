36c428673a1d366d2826499740ef50a922dc0e4f
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-24 15:01:47 +0300

    async config

---
+++ achievement_service/src/main/java/com/clone/twitter/achievement_service/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.clone.twitter.achievement_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+        return executor;
+    }
+}
