729d05aef1e28d6fe15aaebb9f2fbe455fec4cc6
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-08-24 15:01:23 +0300

    Introduced feign client interceptor

---
+++ achievement_service/src/main/java/com/clone/twitter/achievement_service/client/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.clone.twitter.achievement_service.client;
+
+import com.clone.twitter.achievement_service.contex.UserContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public FeignUserInterceptor feignUserInterceptor(UserContext userContext) {
+        return new FeignUserInterceptor(userContext);
+    }
+}
---
+++ achievement_service/src/main/java/com/clone/twitter/achievement_service/client/FeignUserInterceptor.java
@@ -0,0 +1,17 @@
+package com.clone.twitter.achievement_service.client;
+
+import com.clone.twitter.achievement_service.contex.UserContext;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FeignUserInterceptor implements RequestInterceptor {
+
+    private final UserContext userContext;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        template.header("twitter-user-id", String.valueOf(userContext.getUserId()));
+    }
+}
