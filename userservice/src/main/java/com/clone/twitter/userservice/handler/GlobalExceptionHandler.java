29058b5b90d7edbd0e3c6f172c71ea7c6b689715
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-01 22:14:14 +0300

    handler

---
+++ userservice/src/main/java/com/clone/twitter/userservice/handler/GlobalExceptionHandler.java
@@ -0,0 +1,70 @@
+package com.clone.twitter.userservice.handler;
+
+import com.clone.twitter.userservice.exception.DataValidationException;
+import com.clone.twitter.userservice.exception.ErrorResponse;
+import com.clone.twitter.userservice.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataValidationException(DataValidationException e, HttpServletRequest request) {
+        log.error("Data validation error: {}", e.getMessage());
+        return buildErrorResponse(e, request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                ));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(NotFoundException e, HttpServletRequest request) {
+        log.error("Not found: {}", e.getMessage());
+        return buildErrorResponse(e, request);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.REQUEST_ENTITY_TOO_LARGE)
+    public ErrorResponse handleMaxUploadSizeExceededException(RuntimeException e, HttpServletRequest request) {
+        log.error("Max upload size exceeded exception: {}", e.getMessage(), e);
+        return buildErrorResponse(e, request);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeException(RuntimeException e, HttpServletRequest request) {
+        log.error("Runtime exception: {}", e.getMessage(), e);
+        return buildErrorResponse(e, request);
+    }
+
+    private ErrorResponse buildErrorResponse(Exception e, HttpServletRequest request) {
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .url(request.getRequestURI())
+                .message(e.getMessage())
+                .build();
+    }
+}
