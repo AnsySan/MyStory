8d285e55dd7ec9280c93896f5206b3d519f092a1
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-09 21:35:00 +0300

    refactor

---
+++ postservice/src/main/java/com/clone/twitter/postservice/controller/CommentLikeController.java
@@ -0,0 +1,40 @@
+package com.clone.twitter.postservice.controller;
+
+import com.clone.twitter.postservice.context.UserContext;
+import com.clone.twitter.postservice.dto.CommentLikeDto;
+import com.clone.twitter.postservice.service.like.CommentLikeServiceImpl;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/likes/comment")
+@RequiredArgsConstructor
+public class CommentLikeController {
+
+    private final CommentLikeServiceImpl likeService;
+    private final UserContext userContext;
+
+    @PostMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Parameter(in = ParameterIn.HEADER, name = "x-user-id", required = true)
+    public CommentLikeDto likeComment(@Positive @PathVariable long commentId) {
+        long userId = userContext.getUserId();
+        return likeService.addLike(userId, commentId);
+    }
+
+    @DeleteMapping("/{commentId}")
+    @Parameter(in = ParameterIn.HEADER, name = "x-user-id", required = true)
+    public void deleteLikeFromComment(@Positive @PathVariable long commentId) {
+        long userId = userContext.getUserId();
+        likeService.removeLike(userId, commentId);
+    }
+}
