b3bb7f39542b62e48f5c6449b27fd3ec59fa5392
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-09 21:23:59 +0300

    added like db

--- postservice/src/main/resources/db/changelog/changeset/post_V001__initial.sql
+++ postservice/src/main/resources/db/changelog/changeset/post_V001__initial.sql
@@ -1,12 +1,13 @@
 CREATE TABLE post (
                       id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+                      content varchar(4096) NOT NULL,
                       author_id bigint,
+                      project_id bigint,
                       published boolean DEFAULT false NOT NULL,
                       published_at timestamptz,
                       deleted boolean DEFAULT false NOT NULL,
                       created_at timestamptz DEFAULT current_timestamp,
-                      updated_at timestamptz DEFAULT current_timestamp,
-                      verified boolean NOT NULL
+                      updated_at timestamptz DEFAULT current_timestamp
 );
 
 CREATE TABLE comment (
--- postservice/src/main/resources/db/drop.sql
+++ postservice/src/main/resources/db/drop.sql
@@ -2,3 +2,5 @@ DROP TABLE likes;
 DROP TABLE comment;
 DROP TABLE post;
 DROP TABLE resource_id;
+DROP TABLE comment_likes;
+DROP TABLE post_likes;
