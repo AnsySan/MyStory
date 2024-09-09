8be379e2d49d829db47b055cc1128279192ef811
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-09 21:34:57 +0300

    refactor

---
+++ postservice/src/main/java/com/clone/twitter/postservice/dto/PostLikeDto.java
@@ -0,0 +1,19 @@
+package com.clone.twitter.postservice.dto;
+
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostLikeDto {
+    private Long id;
+    @Positive
+    private Long userId;
+    @Positive
+    private Long postId;
+}
---
+++ postservice/src/main/java/com/clone/twitter/postservice/dto/UserProfilePic.java
@@ -0,0 +1,17 @@
+package com.clone.twitter.postservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder
+@Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfilePic {
+    private String fileId;
+    private String smallFileId;
+}
