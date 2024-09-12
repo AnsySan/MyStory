08bbb0ae62422188a38781bfe021818546b61156
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-12 21:33:09 +0300

    refactor

--- postservice/src/main/java/com/clone/twitter/postservice/PostserviceApplication.java
+++ postservice/src/main/java/com/clone/twitter/postservice/PostserviceApplication.java
@@ -1,5 +1,6 @@
 package com.clone.twitter.postservice;
 
+import com.clone.twitter.postservice.config.redis.RedisCacheConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.Banner;
@@ -8,14 +9,20 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
-@EnableCaching
+@EnableAsync(proxyTargetClass = true)
+@EnableCaching(proxyTargetClass = true)
+@EnableRedisRepositories(
+        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP,
+        keyspaceConfiguration = RedisCacheConfig.RedisKeyspaceConfiguration.class,
+        shadowCopy = RedisKeyValueAdapter.ShadowCopy.OFF)
 @EnableFeignClients(basePackages = "com.clone.twitter.postservice.client")
 @EnableRetry
 public class PostserviceApplication {
