b7393fb8bc088ad8f888b7631da1859b4d3d868e
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-09-30 21:12:50 +0300

    first commit

---
+++ payment_service/src/main/java/com/clone/twitter/payment_service/PaymentApplication.java
@@ -0,0 +1,16 @@
+package com.clone.twitter.payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PaymentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+}
