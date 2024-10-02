25ba305196af22b5a03f707d3aef475f55b5c995
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-10-02 09:59:05 +0300

    first commit

---
+++ payment_service/src/main/java/com/clone/twitter/payment_service/client/OpenExchangeRatesClient.java
@@ -0,0 +1,13 @@
+package com.clone.twitter.payment_service.client;
+
+import com.clone.twitter.payment_service.model.ExchangeRatesResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "openExchangeRatesClient", url = "${feign.openExchangeRatesClient.url}")
+public interface OpenExchangeRatesClient {
+
+    @GetMapping("/latest.json?app_id=${feign.openExchangeRatesClient.appId}")
+    ExchangeRatesResponse getRates();
+
+}
