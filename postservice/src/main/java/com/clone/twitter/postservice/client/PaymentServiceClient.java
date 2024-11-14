dfea115fccf6c5af0697607df5a2fa8f0b6edc73
Evgenij Pashkeciv <epaskevic4@gmail.com>
2024-11-14 11:22:22 +0300

    initial

---
+++ postservice/src/main/java/com/clone/twitter/postservice/client/PaymentServiceClient.java
@@ -0,0 +1,15 @@
+package com.clone.twitter.postservice.client;
+
+import com.clone.twitter.postservice.dto.payment.PaymentRequest;
+import com.clone.twitter.postservice.dto.payment.PaymentResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "payment_service", url = "${payment-service.host):${payment-service.port}")
+public interface PaymentServiceClient {
+
+    @PostMapping("/api/payment")
+    ResponseEntity<PaymentResponse> postPayment(@RequestBody PaymentRequest paymentRequest);
+}
